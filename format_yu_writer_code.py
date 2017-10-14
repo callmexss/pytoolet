2a7fd1a779df03ab5ee4b2e86b2433f54f3fe7f5
callmexss <callmexss@126.com>
2017-10-14 15:10:28 +0800

    first srcipt

--- format_yu_writer_code.py
+++ format_yu_writer_code.py
@@ -1,7 +1,30 @@
+import clipboard
+import time
+import os
 
-s = '''
-some code
-'''
+root_path = os.getcwd()
+
+data_path = os.path.join(root_path, r'data/')
+if not os.path.isdir(data_path):
+    os.mkdir(data_path)
 
 
-s.replace('\t', '    ').replace('\n\n', '\n')
+file_path = os.path.join(data_path, 'yu_code_format.txt')
+
+os.system("notepad " + file_path)
+
+lines = []
+
+
+with open(file_path, "r") as f:
+    lines = f.readlines()
+
+s = ''.join(lines)
+
+
+res = s.replace('\t', '    ').replace('\n\n', '\n')
+clipboard.copy(res)
+
+
+with open(file_path, "w+") as f:
+    lines = f.write('')
